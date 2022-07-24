19f70f424effecb5365c62607aaa4a5f86ff7b24
Cao Nam <caonam.uit@gmail.com>
2022-07-24 11:31:42 +0700

    end section 2

--- Section Two Basics & Core Concepts/basics-05-using-the-native-event-object/app.js
+++ Section Two Basics & Core Concepts/basics-05-using-the-native-event-object/app.js
@@ -2,10 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      // fullname: '',
+      lastName: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
+    name(value) {
+      if (value === '') {
+        console.log('nam cao 1');
+        this.fullname = '';
+      } else {
+        console.log('nam cao 2');
+        this.fullname = value + ' ' + 'Nam cao';
+      }
+    },
+    lastName(value) {
+      if (value === '') {
+        console.log('nam cao 3');
+        this.fullname = '';
+      } else {
+        console.log('nam cao 4');
+        this.fullname = this.name + ' ' + value;
+      }
+    }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName + '1';
+    }
+  },
   methods: {
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Nam Cao';
+    },
     setName(event) {
       this.name = event.target.value;
     },
