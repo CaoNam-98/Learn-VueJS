7ee0ed6d33c3ca314b440608d5d2f8755f4374a9
Cao Nam <caonam.uit@gmail.com>
2022-08-20 18:56:30 +0700

    section 7,8

---
+++ Section 8 Component Communication/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <!-- Khi click vào button này sẽ thực thi gọi hàm selectTopic trong provide của App() do mình có Inject-->
+    <button @click="selectTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['selectTopic'],
+  props: ['id', 'topicName', 'description'],
+  emits: ['select-topic'],
+};
+</script>
---
+++ Section Seven/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+h2 {
+  color: red;
+}
+</style>
